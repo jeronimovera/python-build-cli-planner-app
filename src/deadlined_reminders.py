384af1094239dc0ad6c77dfbc882898c64a57511
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-21 15:57:53 -0300

    asd

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
-    def is_due():
+    def is_due(self):
         pass
 
 class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
-    def is_due():
+    def is_due(self):
         pass
     
     @classmethod
